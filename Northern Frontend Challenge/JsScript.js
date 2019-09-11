e01dc7193d1597c722cd6212705a01380edefcc1
wncks0609 <wncks0609@hotmail.com>
2019-09-11 15:29:41 -0700

    done

--- Northern Frontend Challenge/JsScript.js
+++ Northern Frontend Challenge/JsScript.js
@@ -1,6 +1,19 @@
 function submit() {
     var email = $("#email").val();
     var validation = validateEmail(email);
+
+    if(validation)
+    {
+        $("#submit").text("Submitting...");
+        setTimeout(function() {
+            window.location = "submittedPage.html";
+        },2000);
+
+    }
+    else {
+        $("#error").text("Please enter a vaild email address."); 
+        $("#email").css("border", "3px solid #D02035"); 
+    }
 }
 
 function validateEmail(email)
