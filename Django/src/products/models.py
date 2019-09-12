7b99ed7bb0e0345caa35d95c014d39b2313db8c7
wncks0609 <wncks0609@hotmail.com>
2019-09-12 16:13:40 -0700

    started on Models

--- Django/src/products/models.py
+++ Django/src/products/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model) :
-	title 		= models.TextField() 
-	description = models.TextField()
-	price 		= models.TextField()
+	title 		= models.CharField(max_length=120) #max_length = required. 
+	description = models.TextField(blank = True, null = True)
+	price 		= models.DecimalField(max_digits = 1000 , decimal_places = 2)
 	summary 	= models.TextField(default = 'this is cool')
